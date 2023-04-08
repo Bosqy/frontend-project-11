24a8ff72a07d8d9750138a9ed89f558a3c492910
Bosqy <groo@vth.ru>
2023-04-08 14:35:20 +1000

    try into step 2

--- src/init.js
+++ src/init.js
@@ -1,9 +1,45 @@
 // @ts-check
 
-import Example from './Example.js';
+import { object, string } from 'yup';
+import onChange from 'on-change';
+
+const isValidUrl = (feed) => {
+  const schema = object({ feed: string().url() });
+  return schema.isValid({ feed });
+};
 
 export default () => {
-  const element = document.getElementById('point');
-  const obj = new Example(element);
-  obj.init();
+  const state = {
+    newFeed: null,
+  };
+  const feeds = [];
+
+  const watchedState = onChange(state, () => {
+    const urlInput = document.getElementById('url-input');
+    urlInput.classList.remove('is-invalid');
+
+    isValidUrl(state.newFeed).then((isUrl) => {
+      const hasUrl = feeds.includes(state.newFeed);
+
+      console.log(`${isUrl}  ${hasUrl}`);
+
+      const isValid = isUrl && !hasUrl;
+      if (isValid) {
+        feeds.push(state.newFeed);
+        console.log(feeds);
+        urlInput.value = '';
+        return;
+      }
+      urlInput.classList.add('is-invalid');
+    });
+  });
+  // Controller
+  const rssForm = document.querySelector('.rss-form');
+
+  rssForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const url = formData.get('url');
+    watchedState.newFeed = url.trim();
+  });
 };
