fcfe1f405e94dffd90890ab41182e4b16cd157b9
Bosqy <groo@vth.ru>
2023-04-13 20:30:29 +1000

    fix typo in i18n

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -2,7 +2,7 @@ export default {
   translation: {
     language: 'Русский',
     rssInvalidContent: 'Ресурс не содержит валидный RSS',
-    rssInvalidFormat: 'Ссылка дожна быть валидным URL',
+    rssInvalidFormat: 'Ссылка должна быть валидным URL',
     rssNetworkError: 'RSS недоступен',
     rssExists: 'RSS уже существует',
     rssSuccess: 'RSS успешно загружен',
